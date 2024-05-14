b4a09e0b4b55d7b88fd7c9d33c34a8a13cae155a
LaizaBarbosa <laiza0700@gmail.com>
2024-05-14 10:38:04 -0300

    fix: resolvido erro de formatação da resposta da api

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 from dotenv import load_dotenv
 import os
 import google.generativeai as genai
+import markdown
 
 # config do .env
 load_dotenv()
@@ -32,25 +33,33 @@ safety_settings = [
     "threshold": "BLOCK_MEDIUM_AND_ABOVE"
   },
 ]
+
 model = genai.GenerativeModel('gemini-pro', safety_settings=safety_settings, generation_config=generation_config)
 chat = model.start_chat(history=[]) 
 
 @app.route("/", methods=["GET","POST"])
 def makePrompt():
-    
 
     if request.method == "POST":
         text = request.form["text"]
-        error = "Algo deu errado"
         
-        response = chat.send_message(text, stream=True)
+        response = chat.send_message(text)
         response.resolve()
 
-        result = [message for message in chat.history]
+        result = (message for message in chat.history)
+        if(result):
+            text = request.form["text"].replace(text, '')
+
+        # passando a resposta do gemini com formatação markdown padrão 
+        modelResponse = response.text
+        modelResponse = markdown.markdown(modelResponse)
+                
 
         return render_template(
             "chatTemplate.html",
-            result = result
+            result = result,
+            response = response,
+            modelResponse = modelResponse
         )
     
     else:
